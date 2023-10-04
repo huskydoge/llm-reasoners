d2a16662ba6d13e0349859e25237682ebe842a96
Haotian Luo <aegeanyanlht@gmail.com>
2023-10-04 06:49:41 +0000

    for bash visualize

--- examples/rap_gsm8k/visualize.py
+++ examples/rap_gsm8k/visualize.py
@@ -1,3 +1,4 @@
+from json import load
 import pickle
 # add path
 import sys
@@ -8,11 +9,18 @@ import os
 from reasoners.visualization import visualize
 from reasoners.visualization.tree_snapshot import NodeData
 from reasoners.algorithm import MCTSNode, bDFSNode
+import fire
+import datasets
+from datasets import load_dataset
+def main(idx: int):
+    mcts_result = pickle.load(open(f'/data/haotian/RAP_tune/llm-reasoners/logs/gsm8k_bDFS/09272023-191700/algo_output/{idx}.pkl', 'rb'))
+    def gsm_node_data_factory(x: Union[MCTSNode, bDFSNode]) -> dict:
+        if not x.state:
+            return {}
+        return {"question": x.state[-1].sub_question, "answer": x.state[-1].sub_answer}
+    print(idx)
+    data = load_dataset('gsm8k', "main", split='train')
+    print("The question is:" ,data['question'][idx])
+    visualize(mcts_result, node_data_factory=gsm_node_data_factory)
 
-mcts_result = pickle.load(open('/data/haotian/RAP_tune/llm-reasoners/logs/gsm8k_bDFS/09272023-191700/algo_output/2.pkl', 'rb'))
-def gsm_node_data_factory(x: Union[MCTSNode, bDFSNode]) -> dict:
-    if not x.state:
-        return {}
-    return {"question": x.state[-1].sub_question, "answer": x.state[-1].sub_answer}
-
-visualize(mcts_result, node_data_factory=gsm_node_data_factory)
+fire.Fire(main)
---
+++ view_data.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python examples/rap_gsm8k/visualize.py --idx $1
