3d10ba2ced015416592ebeacf55540783bad6fa3
Haotian Luo <aegeanyanlht@gmail.com>
2023-10-19 04:10:40 +0000

    stash

--- examples/rap_gsm8k/perturb_answer_check.py
+++ examples/rap_gsm8k/perturb_answer_check.py
@@ -1,9 +1,42 @@
 import pickle
 from datasets import Dataset
 import utils
+from reasoners.algorithm.beam_dfs import bDFSNode, bDFSResult
 RESUME_B = 0
-RESUME_E = 100
-
+RESUME_E = 16
+def increase_q(node: bDFSNode):
+    if node.parent is None:
+        node.q += 1
+        return
+    else:
+        node.q += 1
+        increase_q(node.parent)
 data = Dataset.from_json('/data/haotian/RAP_tune/llm-reasoners/perturb_data.json')
-for i in range(RESUME_B, RESUME_E):
-    tree_log = 
+def dfs_perturb_answer(node: bDFSNode, data_idx: int):##since the formmer answer is old answer to check the leakage
+    flag = False
+    node.q = 0
+    if node.is_terminal:
+        perturb_answer = utils.retrieve_answer_from_dataset_perturb(data['answer'][data_idx])
+        output_answer = utils.retrieve_answer(node.state[-1].sub_question)
+        correct = utils.judge_answer(output_answer, perturb_answer)
+        if correct:
+            increase_q(node) 
+            flag = True
+        return flag
+    else:
+        for child in node.children:
+            flag |= dfs_perturb_answer(child, data_idx)
+    
+    return flag
+correct_count = 0
+for idx in range(RESUME_B, RESUME_E):
+    bdfs_result = pickle.load(open(f'/data/haotian/RAP_tune/llm-reasoners/logs/gsm8k_bDFS/10172023-132150/algo_output/{idx+1}.pkl','rb'))
+    root = bdfs_result.tree_state
+    correct = dfs_perturb_answer(root, idx)
+    correct_count += int(correct)
+    accuracy = correct_count / (idx + 1)
+    log_str = f'Case #{RESUME_B + idx + 1}: {correct=}; {accuracy=:.3f} ({correct_count}/{idx + 1})'
+    with open(f'/data/haotian/RAP_tune/llm-reasoners/logs/gsm8k_bDFS/perturbed/result.log','a') as f:
+        print(log_str, file=f)
+    with open(f'/data/haotian/RAP_tune/llm-reasoners/logs/gsm8k_bDFS/perturbed/algo_output/{idx+1}.pkl', 'wb') as f:
+        pickle.dump(bdfs_result, f)
--- examples/rap_gsm8k/visualize.py
+++ examples/rap_gsm8k/visualize.py
@@ -7,11 +7,10 @@ from typing import Union
 import os
 # print(os.cwd())
 from reasoners.visualization import visualize
-from reasoners.visualization.tree_snapshot import NodeData
 from reasoners.algorithm import MCTSNode, bDFSNode
 import fire
 import datasets
-from datasets import load_dataset
+from datasets import load_dataset, Dataset
 def main(idx: int):
     # mcts_result = pickle.load(open(f'/data/haotian/RAP_tune/llm-reasoners/logs/gsm8k_bDFS/question_given/10042023-172629/algo_output/507.pkl', 'rb'))
     # mcts_result = pickle.load(open(f'/data/haotian/RAP_tune/llm-reasoners/logs/gsm8k_bDFS/10072023-163332/algo_output/{idx}.pkl', 'rb'))#cleaned more
@@ -23,7 +22,8 @@ def main(idx: int):
             return {}
         return {"question": x.state[-1].sub_question}
     print(idx)
-    data = load_dataset('gsm8k', "main", split='train')
+    # data = load_dataset('gsm8k', "main", split='train')
+    data = Dataset.from_json('/data/haotian/RAP_tune/llm-reasoners/perturb_data.json')
     print("The question is:" ,data['question'][idx-1])
     print("The answer is:" ,data['answer'][idx-1])
     visualize(mcts_result, node_data_factory=gsm_node_data_factory)
